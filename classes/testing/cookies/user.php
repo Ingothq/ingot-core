23eeda04d932680ba17872a2fd72b78c12ac27c8
Josh Pollock <jpollock412@gmail.com>
2016-07-08 20:56:09 -0400

    #10 extend the right class

--- classes/testing/cookies/user.php
+++ classes/testing/cookies/user.php
@@ -1,20 +1,4 @@
-<?php
-/**
- * User cookie
- *
- * @package   ingot
- * @author    Josh Pollock <Josh@JoshPress.net>
- * @license   GPL-2.0+
- * @link
- * @copyright 2015 Josh Pollock
- */
-
-namespace ingot\testing\cookies;
-
-
-use ingot\testing\crud\session;
-
-class user extends cookie {
+class user extends init {
 
 
 	/**
